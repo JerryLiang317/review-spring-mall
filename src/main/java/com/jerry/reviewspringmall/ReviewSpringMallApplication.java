9804107f280d6329ef809ead94a33fcc377912be
JerryLiang317 <a0988768311@gmail.com>
2023-09-14 23:21:37 +0800

    初始化 spring boot project

---
+++ src/main/java/com/jerry/reviewspringmall/ReviewSpringMallApplication.java
@@ -0,0 +1,13 @@
+package com.jerry.reviewspringmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewSpringMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReviewSpringMallApplication.class, args);
+    }
+
+}
